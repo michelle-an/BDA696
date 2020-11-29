75bf40a4d3355a44628c99c385e9cfafc3d08294
Julien Pierret <dafrenchyman@gmail.com>
2020-11-29 12:20:58 -0800

    Great job! I changed things around to make it a little cleaner.
    This should help you on the final a bit more doing it this way. I'm also already using the mariadb name on my computer and had to change it everywhere for it to work. This will make it so I can run everyone's stuff locally.

--- docker_assignment/run.sh
+++ docker_assignment/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "    Connecting to mariadb container from ubuntu container"
+sleep 10
+# mysql -h mariadb_michelle -u root -pmichelle123
 
-# mysql -h mariadb -u root -pmichelle123
-
-if ! mysql -h mariadb -u root -pmichelle123 -e 'USE baseballdb'; then
+if ! mysql -h mariadb_michelle -u root -pmichelle123 -e 'USE baseballdb'; then
     echo "    Could not find baseballdb. Creating data base"
-    mysqladmin -u root -h mariadb -pmichelle123 create baseballdb
+    mysqladmin -u root -h mariadb_michelle -pmichelle123 create baseballdb
     echo "    Importing database from ubuntu container"
-    mysql -u root -pmichelle123 -h mariadb --database=baseballdb < baseball.sql
+    mysql -u root -pmichelle123 -h mariadb_michelle --database=baseballdb < baseball.sql
 fi
 
 echo "    Success!"
 echo "    Calculating game summaries"
 # initial calculation of all players hits and at bats for each game, along with the game date
-mysql -h mariadb -u root -pmichelle123 -e '
+mysql -h mariadb_michelle -u root -pmichelle123 -e '
  USE baseballdb;
  CREATE TABLE IF NOT EXISTS rolling_avg_temp
 	SELECT bc.game_id, bc.batter, bc.Hit, bc.atbat, gt.local_date
@@ -24,7 +24,7 @@ mysql -h mariadb -u root -pmichelle123 -e '
 
 echo "    Calculating players in target game"
 # finding target players for selected game and the game date
-mysql -h mariadb -u root -pmichelle123 -e '
+mysql -h mariadb_michelle -u root -pmichelle123 -e '
  USE baseballdb;
  CREATE TABLE IF NOT EXISTS target_players
 	SELECT  bc.batter, gt.local_date
@@ -35,14 +35,14 @@ mysql -h mariadb -u root -pmichelle123 -e '
 
 echo "    Filtering for only target players"
 # using target players table to filter initial calculation for only interested players and dates before target game
-mysql -h mariadb -u root -pmichelle123 -e '
+mysql -h mariadb_michelle -u root -pmichelle123 -e '
  USE baseballdb; 
  CREATE TABLE IF NOT EXISTS filtered_avg
        SELECT * FROM rolling_avg_temp where batter in (select tp.batter from target_players tp);'
 
 echo "    Calculating rolling 100 day batting avreage"
 # calculating 100 day rolling average
-mysql -h mariadb -u root -pmichelle123 -e '
+mysql -h mariadb_michelle -u root -pmichelle123 -e '
  USE baseballdb;
 
  CREATE TABLE IF NOT EXISTS filtered2
@@ -55,12 +55,6 @@ mysql -h mariadb -u root -pmichelle123 -e '
 echo "    Success!!!!"
 
 # writing output
-mysql -h mariadb -u root -pmichelle123 -e '
+mysql -h mariadb_michelle -u root -pmichelle123 -e '
   USE baseballdb;
-  SELECT * INTO OUTFILE "./outputfile.txt"
-  FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY """"
-  LINES TERMINATED BY "\n"
-  FROM rolling_avg;'
-
-
-# cat ./output/outputfile.txt
+  SELECT * FROM rolling_avg;' > /app/output/results.txt
